3b29663dfb62b2b267a94c5908fc183063e3e881
Joeonome <joseph.onumeguolor@gmail.com>
2025-01-30 14:31:08 +0100

    added vercel and api folder

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const cors = require('cors');
+
+app.use(cors());
 
 const dateObj = new Date();
 const jsonString = dateObj.toISOString();
@@ -13,3 +16,6 @@ app.get('/', (req, res) => { res.json({
   "email": "joseph.onumeguolor@gmail.com",
   "current_datetime": jsonString,
   "github_url": "<https://github.com/Joeonome/simpleAPI>"}) });
+
+
+  module.exports = app;
